4deb6f2eb972b70cf015ee1da3542c0ab063a63e
Charlotte <charlotterushen@hotmail.com>
2020-01-23 16:51:39 +0000

    Footer content

---
+++ components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer id="footer">
+    <div class="content">
+      <h1>logo.</h1>
+        <div class="footer-content">
+          <div class="page-links">
+            <nuxt-link to="/">Home</nuxt-link>
+            <nuxt-link to="/work">Work</nuxt-link>
+            <nuxt-link to="/contact">Contact</nuxt-link>
+          </div>
+          <p>&copy;2020 Charlotte Rushen. All rights reserved.</p>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  background: var(--off-white);
+  color: var(--dark-grey);
+  padding: 32px 0;
+}
+
+.page-links {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 16px;
+}
+
+a, p {
+  color: var(--grey);
+  font-size: 14px;
+  line-height: 28px;
+}
+
+p {
+  margin-top: 32px;
+}
+</style>
