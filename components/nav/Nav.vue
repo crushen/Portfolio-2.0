60d6e3667dae14096de6947c1f1da7d3375eec24
Charlotte <charlotterushen@hotmail.com>
2020-01-30 17:48:12 +0000

    Started logic for nav

---
+++ components/nav/Nav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="nav">
+    <nav>
+      <nuxt-link to="/" @click="toggleNav">Home</nuxt-link>
+      <nuxt-link to="/work" @click="toggleNav">Work</nuxt-link>
+      <nuxt-link to="/contact" @click="toggleNav">Contact</nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    toggleNav: { type: Function, required: true }
+  }
+}
+</script>
+
+<style scoped>
+#nav {
+  height: 100vh;
+  width: 75%;
+  background: #81d4fa;
+  position: absolute;
+  top: 0;
+  right: 0;
+
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+  margin: 100px 0 0 32px;
+}
+
+a {
+  font-size: 20px;
+  line-height: 50px;
+  color: var(--white);
+}
+
+.nuxt-link-exact-active {
+  color: var(--black);
+}
+</style>
--- components/nav/Navbar.vue
+++ components/nav/Navbar.vue
@@ -1,18 +1,54 @@
 <template>
-  <header>
-    <nuxt-link to="/">
-      <h1>logo.</h1>
-    </nuxt-link>
-    <div class="nav-button">
-      <div class="nav-line"></div>
-      <div class="nav-line"></div>
-      <div class="nav-line"></div>
-    </div>
-  </header>
+  <div>
+    <header>
+      <nuxt-link to="/">
+        <h1>logo.</h1>
+      </nuxt-link>
+      <button
+        @click="toggleNav"
+        class="nav-button">
+        <div class="nav-line"></div>
+        <div class="nav-line"></div>
+        <div class="nav-line"></div>
+      </button>
+    </header>
+
+    <!-- Make this not it's own component -->
+    <my-nav 
+      v-if="active" 
+      v-on-clickaway="closeNav" 
+      :toggleNav="toggleNav" />
+  </div>
 </template>
+
+<script>
+import { mixin as clickaway } from 'vue-clickaway';
+import myNav from '@/components/nav/Nav'
+
+export default {
+  mixins: [ clickaway ],
+  components: { myNav },
+  data() {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    toggleNav() {
+      this.active = !this.active;
+      console.log(this.active);
+    },
+    closeNav() {
+      this.active = false;
+    }
+  }
+}
+</script>
 
 <style scoped>
 header {
+  position: fixed;
+  width: 100%;
   padding: 10px 5%;
   background: var(--black);
   color: var(--white);
@@ -22,6 +58,8 @@ header {
 }
 
 .nav-button {
+  background: none;
+  border: none;
   height: 18px;
   width: 25px;
   cursor: pointer;
