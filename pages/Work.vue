ac447cdcd58d396e57dd0a67a59827462873770e
Charlotte <charlotterushen@hotmail.com>
2020-02-06 17:36:54 +0000

    Added title for page

--- pages/Work.vue
+++ pages/Work.vue
@@ -1,9 +1,21 @@
 <template>
-  <h1>Work</h1>
+  <section id="work">
+    <div class="content">
+      <div class="work-title">
+        <h1 class="page-sub-title">Work</h1>
+        <h2 class="title page-title">Take a look at some of projects I've been working on recently.</h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-h1 {
+.work-title {
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
   padding-top: 100px;
 }
 </style>
