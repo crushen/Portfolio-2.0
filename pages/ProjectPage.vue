1df4adc4e4d1ea38d3f147e719e2a969f2cc45a0
Charlotte <charlotterushen@hotmail.com>
2020-02-13 11:14:34 +0000

    Created project page

---
+++ pages/ProjectPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <section id="work">
+    <div class="content">
+      <div class="work-title">
+        <h1 class="page-sub-title">Project Name</h1>
+        <h2 class="title page-title">Description of project.</h2>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
