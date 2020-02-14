ac2de1a78d6629553007239e108c455b598af33b
Charlotte <charlotterushen@hotmail.com>
2020-02-14 13:12:14 +0000

    Added dynamic files to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,29 @@
+const projects = [
+  {
+    title: 'Meahė Design',
+    subTitle: "Taiwanese creative design company that's based in London.",
+    thumbnail: 'meahe-logo.png',
+    slug: 'meahe-design'
+  },
+  {
+    title: 'Chord Progressions',
+    subTitle: 'An app that randomly generates chord progressions.',
+    thumbnail: 'guitar.jpg',
+    slug: 'chord-progressions'
+  },
+  {
+    title: 'Emma Kate Parkinson',
+    subTitle: 'Online portfolio for a UI/UX Designer and Creative.',
+    thumbnail: 'emma-logo.png',
+    slug: 'emma-kate-parkinson'
+  }
+]
+
+let dynamicRoutes = () => {
+ return new Promise(resolve => {
+   resolve(projects.map(item => `work/${item.slug}`))
+ })
+}
 
 export default {
   mode: 'universal',
@@ -15,6 +41,9 @@ export default {
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Catamaran:400,700|Vesper+Libre:900&display=swap' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css' }
     ]
+  },
+  generate: {
+    routes: dynamicRoutes
   },
   plugins: [],
   buildModules: [],
