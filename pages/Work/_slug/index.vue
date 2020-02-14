da7b2750242f0457f6d2f597a56ee49d580475db
Charlotte <charlotterushen@hotmail.com>
2020-02-14 12:59:04 +0000

    Project page dynamically displaying data

--- pages/Work/_slug/index.vue
+++ pages/Work/_slug/index.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1>{{ project.subTitle }}</h1>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      slug: this.$route.params.slug
+    }
+  },
+  computed: {
+    ...mapState(["data"]),
+    project() {
+      return this.data.find(item => item.slug === this.slug);
+    }
+  }
+}
+</script>
 
 <style scoped>
 h1 {
