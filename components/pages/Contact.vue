483a15607d30d814cb193e7177f124099c497f58
Charlotte Rushen <charlotterushen@hotmail.com>
2020-07-22 09:58:53 +0100

    target blank on mailto

--- components/pages/Contact.vue
+++ components/pages/Contact.vue
@@ -4,7 +4,7 @@
     <h2 class="title page-title">Have any enquiries, or just want to say hello?</h2>
 
     <div class="page-text">
-      <p>You can send me an email at <a href="mailto:hello@charlotterushen.com">hello@charlotterushen.com</a>.</p>
+      <p>You can send me an email at <a href="mailto:hello@charlotterushen.com" target="_blank">hello@charlotterushen.com</a>.</p>
       <p>Or you can follow me on <a href="https://github.com/crushen" target="_blank">GitHub</a>, <a href="https://twitter.com/charlotterushen" target="_blank">Twitter</a>, <a href="https://codepen.io/charlotterushen" target="_blank">Codepen</a> or <a href="https://www.linkedin.com/in/charlotte-rushen" target="_blank">LinkedIn</a>.</p>
     </div>
   </section>
